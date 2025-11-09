4b257b28f8dd39471221c3e47778c0f4b7ed1e18
Yashraj Chouhan <yashrajchouhan14@gmail.com>
2025-11-09 14:43:12 +0530

    Implemented get Route

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -29,3 +29,41 @@ export const updateUser = async (req, res) => {
       .json({ message: "Internal server error.", error: error.message });
   }
 };
+
+export const getUser = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        name: true,
+        userName: true,
+        email: true,
+        imgUrl: true,
+        annonymousImgUrl: true,
+        gender: true,
+        phone: true,
+        hostel: true,
+        roomNumber: true,
+        isVerified: true,
+        isProfileAnonymous: true,
+        academicYear: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ message: "User fetched successfully", user });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error.", error: error.message });
+  }
+};
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { updateUser } from "../controllers/userController.js"
+import { updateUser, getUser } from "../controllers/userController.js"
 import { verifyToken } from "../middlewares/authMiddleware.js"
 
 const router = express.Router()
 
+router.get('/profile', verifyToken, getUser)
 router.put('/profile',verifyToken,updateUser)
 
 export default router
